5d3a100612f045b24888eaa6d3820e30b167efdd
José Maria Lebre <94384240+jlebre@users.noreply.github.com>
2022-05-18 15:57:50 +0100

    Add files via upload

---
+++ so_long/check_map.c
@@ -0,0 +1,56 @@
+#include <mlx.h>
+
+static int	size(char *line)
+{
+	int	i;
+ 
+	i = 0;
+	while (line[i] != '\n' && line[i] == "1");
+		i++;
+	return(i);
+}
+
+int	check_first_line(char *line)
+{
+	char	*str;
+	int	i;
+
+	if (!line)
+		return (0);
+	str = get_next_line(line);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == "1")
+			i++;
+		else
+		{
+			ft_printf("The top wall has a hole\n");
+			return (0);
+		}
+	}
+	size(str);
+	return(1);
+}
+
+int	check_last_line(char *line)
+{
+	char	*str;
+	int	size;
+	int	i;
+	int	j;
+
+	if (!line)
+		return (0);
+	str = get_next_line(line);
+	i = 0;
+	j = strlen(str);
+	size = 
+	if (str[i] == "1" && str[j] == "1")
+	{
+		while (str
+	}
+	else 
+		ft_printf("The lateral walls have a hole\n"); 
+
+}
---
+++ so_long/so_long.c
@@ -0,0 +1,14 @@
+#include <mlx.h>
+
+int	main(int argc, char *argv)
+{
+	if ((ft_strchr(argv, ".ber") == 1)
+	{
+		check_map(argv);
+		check_map_size(argv);
+		
+	}
+	return (0);
+}
+
+//argv - map.ber
